e96f95e94b99c843722bcf1e1229c214ea51bb52
Ghassan Shahzad <ghass.shahzad@gmail.com>
2024-01-06 18:53:39 +0500

    feat: :sparkles: Completely changed site

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
